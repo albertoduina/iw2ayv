591237399fdfff651ef1a4f6bee685c71a33fbb5
Alberto Duina <albertoduina@virgilio.it>
2011-12-22 19:21:22 +0100

    Quasi completo e funzionante MyFwhm

--- Iw2ayv/src/utils/MyFwhm.java
+++ Iw2ayv/src/utils/MyFwhm.java
@@ -10,9 +10,9 @@ import java.awt.Font;
 
 public class MyFwhm {
 
-	
 	/***
 	 * Analisi del profilo, per calcolare l'FWHM
+	 * 
 	 * @param profi1
 	 * @param dimPixel
 	 * @param invert
@@ -28,10 +28,10 @@ public class MyFwhm {
 		} else {
 			profi2 = profi1;
 		}
-		double[] profi3 = smooth3(profi2, 10);
-		int[] vetHalfPoint = halfPointSearch(profi3);
-		double fwhm = calcFwhm(vetHalfPoint, profi3) * dimPixel;
-		double peak = peakPosition(profi3);
+		// double[] profi3 = smooth3(profi2, 10);
+		int[] vetHalfPoint = halfPointSearch(profi2);
+		double fwhm = calcFwhm(vetHalfPoint, profi2) * dimPixel;
+		double peak = peakPosition(profi2);
 		double[] outFwhm = new double[2];
 		outFwhm[0] = fwhm;
 		outFwhm[1] = peak;
@@ -48,7 +48,7 @@ public class MyFwhm {
 	 *            profilo di cui calcolare l'FWHM
 	 * @return FWHM calcolata in pixels
 	 */
-	private static double calcFwhm(int[] vetUpDwPoints, double[] profile) {
+	public static double calcFwhm(int[] vetUpDwPoints, double[] profile) {
 
 		double[] a = Tools.getMinMax(profile);
 		double min = a[0];
@@ -61,7 +61,9 @@ public class MyFwhm {
 		double py1 = profile[vetUpDwPoints[1]];
 		double py2 = (max - min) / 2.0 + min;
 
-		double sx = linearInterpolation(px0, py0, px1, py1, py2);
+		double sx = xLinearInterpolation(px0, py0, px1, py1, py2);
+
+		// IJ.log("punto interpolato a sx= " + sx);
 		// px2 = px0 + (px1 - px0) / (py1 - py0) * (py2 - py0);
 
 		// interpolazione lineare destra
@@ -70,7 +72,8 @@ public class MyFwhm {
 		py0 = profile[vetUpDwPoints[2]];
 		py1 = profile[vetUpDwPoints[3]];
 		py2 = (max - min) / 2.0 + min;
-		double dx = linearInterpolation(px0, py0, px1, py1, py2);
+		double dx = xLinearInterpolation(px0, py0, px1, py1, py2);
+		// IJ.log("punto interpolato a dx= " + dx);
 
 		// px2 = px0 + (px1 - px0) / (py1 - py0) * (py2 - py0);
 		double fwhm = dx - sx; // NOTA BENE è in pixels
@@ -85,142 +88,50 @@ public class MyFwhm {
 	 *            profilo
 	 * @return posizione x del picco (in pixel)
 	 */
-	private static double peakPosition(double[] profile) {
+	public static double peakPosition(double[] profile) {
 
 		double[] a = Tools.getMinMax(profile);
-		double min = a[0];
+		double max = a[1];
 		double peakX = Double.NaN;
 		for (int i1 = 0; i1 < profile.length; i1++) {
-			if (profile[i1] == min)
+			if (profile[i1] == max)
 				peakX = i1;
 		}
 		return (peakX);
 	}
 
-	/**
-	 * Mostra a video un profilo con linea a metà picco
-	 * 
-	 * @param profile1
-	 *            Vettore con il profilo da analizzare
-	 * @param bslab
-	 *            Flag slab che qui mettiamo sempre true
-	 */
+	public static void minimalPlot(double[] profi1, int[] upDwPoints,
+			String title, String text1, String text2, double value,
+			boolean verbose) {
 
-	private static void createPlot(double profile1[], double dimPixel,
-			boolean bslab, boolean bLabelSx, boolean verbose) {
-
-		int[] vetUpDwPoints = halfPointSearch(profile1);
-		double[] a = Tools.getMinMax(profile1);
+		double[] xcoord1 = new double[profi1.length];
+		for (int i1 = 0; i1 < profi1.length; i1++) {
+			xcoord1[i1] = (double) i1;
+		}
+		double[] a = Tools.getMinMax(profi1);
 		double min = a[0];
 		double max = a[1];
-		double xmax = 0;
-		double half = (max - min) / 2.0 + min;
+		Plot plot1 = new Plot(title, text1, text2, xcoord1, profi1);
+		plot1.setColor(Color.BLUE);
+		plot1.show();
+		double half = (max - min) / 2 + min;
+		double[] xHalf = { 0, profi1.length };
+		double[] yHalf = { half, half };
+		plot1.setColor(Color.GREEN);
+		plot1.addPoints(xHalf, yHalf, PlotWindow.LINE);
 
-		double[] xVectPointsM = new double[1];
-		double[] yVectPointsM = new double[1];
-		yVectPointsM[0] = max;
-
-		double[] xVectPointsX = new double[2];
-		double[] yVectPointsX = new double[2];
-
-		double[] xVectPointsO = new double[2];
-		double[] yVectPointsO = new double[2];
-
-		double[] xVetLineHalf = new double[2];
-		double[] yVetLineHalf = new double[2];
-		int len1 = profile1.length;
-		double[] xcoord1 = new double[len1];
-		for (int j1 = 0; j1 < len1; j1++) {
-			xcoord1[j1] = j1;
-			if (profile1[j1] == max) {
-				xmax = (double) j1;
-			}
-		}
-		xVectPointsM[0] = xmax;
-
-		// PlotWindow plot = new PlotWindow("Plot profilo penetrazione",
-		// "pixel",
-		// "valore", xcoord1, profile1);
-		xVectPointsX[0] = (double) vetUpDwPoints[0];
-		xVectPointsX[1] = (double) vetUpDwPoints[2];
-		yVectPointsX[0] = profile1[vetUpDwPoints[0]];
-		yVectPointsX[1] = profile1[vetUpDwPoints[2]];
-		xVectPointsO[0] = (double) vetUpDwPoints[1];
-		xVectPointsO[1] = (double) vetUpDwPoints[3];
-		yVectPointsO[0] = profile1[vetUpDwPoints[1]];
-		yVectPointsO[1] = profile1[vetUpDwPoints[3]];
-
-		// interpolazione lineare sinistra
-		double px0 = vetUpDwPoints[0];
-		double px1 = vetUpDwPoints[1];
-		double px2 = 0;
-		double py0 = profile1[vetUpDwPoints[0]];
-		double py1 = profile1[vetUpDwPoints[1]];
-		double py2 = 0;
-		py2 = half;
-		px2 = px0 + (px1 - px0) / (py1 - py0) * (py2 - py0);
-		double sx = px2;
-		// interpolazione lineare destra
-		px0 = vetUpDwPoints[2];
-		px1 = vetUpDwPoints[3];
-		py0 = profile1[vetUpDwPoints[2]];
-		py1 = profile1[vetUpDwPoints[3]];
-		py2 = half;
-		px2 = px0 + (px1 - px0) / (py1 - py0) * (py2 - py0);
-		double dx = px2;
-		double fwhm = (dx - sx) * dimPixel;
-		double labelPosition = 0;
-
-		Plot plot = new Plot("P R O F I L O", "pixel", "valore", xcoord1,
-				profile1);
-		plot.setLimits(0, len1, min, max * 1.05);
-		plot.setLineWidth(1);
-		plot.setColor(Color.RED);
-		plot.addPoints(xVectPointsM, yVectPointsM, PlotWindow.TRIANGLE);
-
-		plot.setColor(Color.RED);
-		plot.addPoints(xVectPointsX, yVectPointsX, PlotWindow.CIRCLE);
-		plot.addPoints(xVectPointsO, yVectPointsO, PlotWindow.CIRCLE);
-		plot.changeFont(new Font("Helvetica", Font.PLAIN, 10));
-
-		if (bLabelSx)
-			labelPosition = 0.10;
-		else
-			labelPosition = 0.60;
-
-		if (verbose) {
-			plot.addLabel(labelPosition, 0.45,
-					"peak / 2=   " + IJ.d2s(max / 2, 2));
-			plot.addLabel(labelPosition, 0.50, "down sx " + vetUpDwPoints[0]
-					+ "  =   " + IJ.d2s(profile1[vetUpDwPoints[0]], 2));
-			plot.addLabel(labelPosition, 0.55, "down dx " + vetUpDwPoints[2]
-					+ "  =   " + IJ.d2s(profile1[vetUpDwPoints[2]], 2));
-			plot.addLabel(labelPosition, 0.60, "up      sx " + vetUpDwPoints[1]
-					+ "  =   " + IJ.d2s(profile1[vetUpDwPoints[1]], 2));
-			plot.addLabel(labelPosition, 0.65, "up      dx " + vetUpDwPoints[3]
-					+ "  =   " + IJ.d2s(profile1[vetUpDwPoints[3]], 2));
-			plot.addLabel(labelPosition, 0.70,
-					"sx interp       =  " + IJ.d2s(sx, 2));
-			plot.addLabel(labelPosition, 0.75,
-					"dx interp       =  " + IJ.d2s(dx, 2));
-			plot.addLabel(labelPosition, 0.80,
-					"fwhm            =  " + IJ.d2s(fwhm, 2));
-		}
-		plot.setColor(Color.PINK);
-		xVetLineHalf[0] = 0;
-		xVetLineHalf[1] = len1;
-		yVetLineHalf[0] = half;
-		yVetLineHalf[1] = half;
-		plot.addPoints(xVetLineHalf, yVetLineHalf, PlotWindow.LINE);
-		plot.setColor(Color.GREEN);
-		PlotWindow pwin = plot.show();
-		// float[] aaa = pwin.getXValues();
-		// MyLog.logVector(aaa, "aaa");
-		// float[] bbb = pwin.getYValues();
-		// MyLog.logVector(bbb, "bbb");
-
-		// plot.draw();
-	} // createPlot2
+		double[] xUpDwPoints = { upDwPoints[0], upDwPoints[1], upDwPoints[2],
+				upDwPoints[3] };
+		double[] yUpDwPoints = { profi1[upDwPoints[0]], profi1[upDwPoints[1]],
+				profi1[upDwPoints[2]], profi1[upDwPoints[3]] };
+		plot1.setColor(Color.RED);
+		plot1.addPoints(xUpDwPoints, yUpDwPoints, PlotWindow.CIRCLE);
+		plot1.changeFont(new Font("Arial", Font.PLAIN, 22));
+		double xlabelPosition = 0.05;
+		double ylabelPosition = 0.15;
+		plot1.addLabel(xlabelPosition, ylabelPosition,
+				"fwhm= " + IJ.d2s(value, 2));
+	}
 
 	/**
 	 * ricerca dei punti a metà altezza
@@ -230,7 +141,7 @@ public class MyFwhm {
 	 * @return isd[0] punto sotto half a sx, isd[1] punto sopra half a sx,
 	 * @return isd[2] punto sotto half a dx, isd[3] punto sopra half a dx
 	 */
-	private static int[] halfPointSearch(double profile1[]) {
+	public static int[] halfPointSearch(double profile1[]) {
 
 		double[] a = Tools.getMinMax(profile1);
 		double min = a[0];
@@ -274,11 +185,7 @@ public class MyFwhm {
 				break;
 			}
 		}
-		int[] vetHalfPoint = new int[4];
-		vetHalfPoint[0] = downSx;
-		vetHalfPoint[1] = upSx;
-		vetHalfPoint[2] = downDx;
-		vetHalfPoint[3] = upDx;
+		int[] vetHalfPoint = { downSx, upSx, downDx, upDx };
 		return vetHalfPoint;
 	}
 
@@ -321,7 +228,7 @@ public class MyFwhm {
 	}
 
 	/**
-	 * Esegue l'interpolazione lineare tra due punti dati
+	 * Esegue l'interpolazione lineare tra due punti dati, conoscendo la x
 	 * 
 	 * @param x0
 	 * @param y0
@@ -330,12 +237,30 @@ public class MyFwhm {
 	 * @param x2
 	 * @return
 	 */
-	public static double linearInterpolation(double x0, double y0, double x1,
+	public static double yLinearInterpolation(double x0, double y0, double x1,
 			double y1, double x2) {
 
 		double y2 = y1 - ((y1 - y0) * (x1 - x2) / (x1 - x0));
 
 		return y2;
+	}
+
+	/**
+	 * Esegue l'interpolazione lineare tra due punti dati, conoscendo la y
+	 * 
+	 * @param x0
+	 * @param y0
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @return
+	 */
+	public static double xLinearInterpolation(double x0, double y0, double x1,
+			double y1, double y2) {
+
+		double x2 = x1 - ((x1 - x0) * (y1 - y2) / (y1 - y0));
+
+		return x2;
 	}
 
 }
---
+++ Iw2ayv/test/utils/MyFwhmTest.java
@@ -0,0 +1,147 @@
+package utils;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Color;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.Plot;
+import ij.gui.PlotWindow;
+import ij.io.Opener;
+import ij.util.Tools;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import utils.MyLog;
+
+public class MyFwhmTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testAnalyzeProfile() {
+
+		double[] profi1 = InputOutput
+				.readDoubleArrayFromFile("./data/vet12.txt");
+		double dimPixel = 0.98765432;
+		boolean invert = true;
+		boolean step = false;
+		// String title = "profi1";
+		// Color color = Color.red;
+		// Plot plot = MyPlot.basePlot(profi1, title, color);
+		// plot.show();
+		// MyLog.waitHere();
+
+		double[] outFwhm = MyFwhm
+				.analyzeProfile(profi1, dimPixel, invert, step);
+		double[] expected = { 30.980876270156863, 65.0 };
+		// MyLog.logVector(outFwhm, "outFwhm");
+		assertTrue(UtilAyv.compareVectors(outFwhm, expected, 1e-12,
+				"errore comparazione"));
+	}
+
+	@Test
+	public final void testHalfPointSearch() {
+
+		double[] profi1 = InputOutput
+				.readDoubleArrayFromFile("./data/vet12.txt");
+		double[] profi2 = MyFwhm.invertProfile(profi1);
+		int[] out2 = MyFwhm.halfPointSearch(profi2);
+		double[] xVectPoints = new double[4];
+		double[] yVectPoints = new double[4];
+		xVectPoints[0] = (double) out2[0];
+		xVectPoints[1] = (double) out2[1];
+		xVectPoints[2] = (double) out2[2];
+		xVectPoints[3] = (double) out2[3];
+		yVectPoints[0] = (double) profi2[out2[0]];
+		yVectPoints[1] = (double) profi2[out2[1]];
+		yVectPoints[2] = (double) profi2[out2[2]];
+		yVectPoints[3] = (double) profi2[out2[3]];
+		// Plot plot2 = MyPlot.basePlot(profi2, "profi2", Color.blue);
+		// plot2.show();
+		// plot2.setColor(Color.RED);
+		// plot2.addPoints(xVectPoints, yVectPoints, PlotWindow.CIRCLE);
+		// MyLog.logVector(xVectPoints, "xVectPoints");
+		// MyLog.logVector(yVectPoints, "yVectPoints");
+		// MyLog.waitHere();
+		int[] expected = { 51, 50, 81, 82 };
+		assertTrue(UtilAyv
+				.compareVectors(out2, expected, "errore comparazione"));
+	}
+
+	@Test
+	public final void testCalcFwhm() {
+
+		double[] profi1 = InputOutput
+				.readDoubleArrayFromFile("./data/vet12.txt");
+		double[] profi2 = MyFwhm.invertProfile(profi1);
+		int[] out2 = MyFwhm.halfPointSearch(profi2);
+		double fwhm = MyFwhm.calcFwhm(out2, profi2);
+		// IJ.log("fwhm=" + fwhm);
+		double expected = 31.36813725490196;
+		assertEquals(expected, fwhm, 1e-12);
+	}
+
+	@Test
+	public final void testPeakPosition() {
+
+		double[] profi1 = InputOutput
+				.readDoubleArrayFromFile("./data/vet12.txt");
+		double[] profi2 = MyFwhm.invertProfile(profi1);
+		double position = MyFwhm.peakPosition(profi2);
+		double expected = 65.0;
+		assertEquals(expected, position, 1e-12);
+	}
+
+	@Test
+	public final void testYLinearInterpolation() {
+
+		double x0 = 51;
+		double y0 = 504;
+		double x1 = 50;
+		double y1 = 453;
+		double x2 = 50.5;
+		double interp = MyFwhm.yLinearInterpolation(x0, y0, x1, y1, x2);
+		double expected = 478.5;
+		assertEquals(expected, interp, 1e-12);
+	}
+
+	@Test
+	public final void testXLinearInterpolation() {
+
+		double x0 = 51;
+		double y0 = 504;
+		double x1 = 50;
+		double y1 = 453;
+		double y2 = 480.5;
+		double interp = MyFwhm.xLinearInterpolation(x0, y0, x1, y1, y2);
+		double expected = 50.53921568627451;
+		assertEquals(expected, interp, 1e-12);
+	}
+
+	@Test
+	public final void testMinimalPlot() {
+
+		double[] profi1 = InputOutput
+				.readDoubleArrayFromFile("./data/vet12.txt");
+		double[] profi2 = MyFwhm.invertProfile(profi1);
+		int[] upDwPoints = MyFwhm.halfPointSearch(profi2);
+
+		MyFwhm.minimalPlot(profi2, upDwPoints, "P L O T", "base", "altezza",
+				5.6789, true);
+		MyLog.waitHere();
+		//
+		// MyFwhm.minimalPlot(profi1);
+	}
+
+}
